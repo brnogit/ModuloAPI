b0143d1a398de8e13b81c80edab8e16f5d156685
Bruno Mota <bmotadev@gmail.com>
2022-11-13 20:55:32 -0300

    Criado Endpoint de Update
    - Adiciona o metodo Atualizar
    - Verifica se o id existe e busca no banco
    - Atualiza informações antigas e salva

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -40,6 +40,27 @@ namespace ModuloAPI.Controllers
 
             return Ok(contato);
         }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)// recebe dois parametros, um com ID do contato a ser atualizado. E o contato/JSON para poder atualizar as informações
+        {
+            var contatoBanco = _context.Contatos.Find(id); // procura o contato por id no banco
+
+            if(contatoBanco == null) //verifica se existe
+            {
+                return NotFound();
+            }
+
+            contatoBanco.Nome = contato.Nome; //vai substituir os dados do contato no banco pelos novos a serem atualizados
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco); //responsavel por atualizar os dados
+            _context.SaveChanges(); //salva as mudanças
+
+            return Ok(contatoBanco);
+        }
     }
 }
 
