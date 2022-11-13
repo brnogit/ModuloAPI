678f3ccd47434f1fcf1d0f9d8ddb1a6e6fd25e4c
Bruno Mota <bmotadev@gmail.com>
2022-11-13 17:28:43 -0300

    Adiciona Entities e Context
    - Adiciona AgendaContext
    - Adiciona Entities Contato
    - Altera cjproj nullable para disable

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+            //recebe a conexão do banco e passa para o base
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+        //entidade: classe no programa, e tabela no banco de dados
+    }
+}
